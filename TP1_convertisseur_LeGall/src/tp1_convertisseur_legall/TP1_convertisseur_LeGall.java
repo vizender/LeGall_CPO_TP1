cde440d6dc57a0c43e84cc3b3c2d7b3e7dfcfe96
Le Gall Victor <victor@looping.org>
2021-10-18 11:18:29 +0200

    Exo 2 ini

---
+++ TP1_convertisseur_LeGall/src/tp1_convertisseur_legall/TP1_convertisseur_LeGall.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_convertisseur_legall;
+
+/**
+ *
+ * @author victorlegall
+ */
+public class TP1_convertisseur_LeGall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
