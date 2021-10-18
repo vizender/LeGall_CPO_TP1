73c31e100fb65459e746e4479b38cd5588db6166
Le Gall Victor <victor@looping.org>
2021-10-18 11:14:38 +0200

    Exo1 finis

--- TP1_manipNombresInt_LeGall/src/tp1_manipnombresint_legall/TP1_manipNombresInt_LeGall.java
+++ TP1_manipNombresInt_LeGall/src/tp1_manipnombresint_legall/TP1_manipNombresInt_LeGall.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_legall;
 
+import java.util.Scanner;
+
 /**
  *
  * @author victorlegall
@@ -14,7 +16,18 @@ public class TP1_manipNombresInt_LeGall {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //On initialise et scan les 2 entiers
+        int n1=0; int n2=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre n1 :");
+        n1=sc.nextInt(); 
+        System.out.println("\n Entrer le nombre n2 :");
+        n2=sc.nextInt(); 
+        
+        //On affcihe somme, diff, produit :
+        System.out.print("somme : "+(n1+n2) + "\ndifference : " + (n1-n2) + "\nproduit : " +(n1*n2) + "\n");
+        //On affiche div euclidienne :
+        System.out.print("Quotient : "+ (n1/n2) + "\nReste : " +(n1%n2) + "\n");
     }
     
 }
