9f1d4bae36044e7d03b4700be2b550ef1337b2ca
Le Gall Victor <victor@looping.org>
2021-10-22 10:10:27 +0200

    Exo 3 V1.2 finis sans cauchemar

--- TP1_guessMyNumber_LeGall/src/tp1_guessmynumber_legall/TP1_guessMyNumber_LeGall.java
+++ TP1_guessMyNumber_LeGall/src/tp1_guessmynumber_legall/TP1_guessMyNumber_LeGall.java
@@ -29,20 +29,43 @@ public class TP1_guessMyNumber_LeGall {
         System.out.println(n1 + " " + n2 + " " +n3+ " " +n4+ " " +n5+ "\n");
         
         //2
+        System.out.println("Choisir difficulté : \n1) Facile \n2) Normal \n3) Difficile");
+        int nc=sc.nextInt();
+        String mode="";
+        switch (nc){
+            case 1:
+                nc=100;
+                mode="Facile";
+                break;
+            case 2:
+                nc=50;
+                break;
+            case 3:
+                nc=10;
+                break;
+        }
         int ncherche = generateurAleat.nextInt(100);
         int nbj = 101;
         int nessais=0;
-        while (nbj!=ncherche){
+        while (nc>0 && nbj!=ncherche){
             nessais+=1;
+            System.out.println(nc + " essais restant, donnez votre guess");
             nbj=sc.nextInt();
             if (nbj<ncherche){
+                if (mode=="Facile" && nbj<ncherche-20){
+                    System.out.println("Vraiment trop petit");
+                }
                 System.out.println("trop petit");
             }
             else if (nbj>ncherche){
+                if (mode=="Facile" && nbj>ncherche+20){
+                    System.out.println("Vraiment trop grand");
+                }
                 System.out.println("Trop Grand");
             }
+            nc-=1;
         }
-        System.out.println("Trouvé en " + nessais + "essais");
+        System.out.println("Trouvé en " + nessais + " essais");
     }
     
 }
