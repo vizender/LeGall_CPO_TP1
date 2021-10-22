fe2800065a12139cb4f2d58773fe67a1b32677f3
Le Gall Victor <victor@looping.org>
2021-10-22 10:15:17 +0200

    Exo 4 v1

---
+++ TP1_stats_LeGall/src/tp1_stats_legall/TP1_stats_LeGall.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_legall;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author victorlegall
+ */
+public class TP1_stats_LeGall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random generateurAleat = new Random();
+        
+        int tab [] = new int[6];
+        int m =sc.nextInt();
+        for (int i=0;i<=m;i++){
+            
+        }
+        
+    }
+    
+}
