81f25359107a0e92e886a0df3031a48fa7d1e941
Le Gall Victor <victor@looping.org>
2021-10-22 11:48:22 +0200

    TP2 exo 2 finis

--- TP2_convertisseurObjet_LeGall/src/tp2_convertisseurobjet_legall/TP2_convertisseurObjet_LeGall.java
+++ TP2_convertisseurObjet_LeGall/src/tp2_convertisseurobjet_legall/TP2_convertisseurObjet_LeGall.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_legall;
 
+import java.util.Scanner;
+
 /**
  *
  * @author victorlegall
@@ -14,7 +16,41 @@ public class TP2_convertisseurObjet_LeGall {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Scanner sc = new Scanner(System.in);
+        
+        Convertisseur convertisseur = new Convertisseur();
+        Convertisseur conversion = new Convertisseur();
+        convertisseur.CelciusVersFahrenheit(-20);
+        convertisseur.KelvinVersCelcius(0);
+        System.out.println(convertisseur);
+        System.out.println("Bonjour, saisissez une valeur : ");
+        double Temp=sc.nextDouble();
+        System.out.println("Saisissez la conversion que vous souhaitez : \n1) Celcius vers Kelvin \n2) Kelvin vers Celcius \n3) Celcius vers Farhenheit \n4) Farenheit vers Celcius \n5) Kelvin vers Farheneit \n6) Farheneit vers Kelvin");
+        int choix=sc.nextInt();
+        
+        switch(choix){
+   
+            case 1:
+                conversion.CelciusVersKelvin(Temp);
+            break;
+            case 2:
+                conversion.KelvinVersCelcius(Temp);
+            break;
+            case 3:
+                conversion.CelciusVersFahrenheit(Temp);
+            break;
+            case 4:
+                conversion.FahrenheitVersCelcius(Temp);
+            break;
+            case 5:
+                conversion.KelvinVersFahrenheit(Temp);
+            break;
+            case 6:
+                conversion.FahrenheitVersKelvin(Temp);
+            break;
+            default:
+            break;
+               
+        }
     
 }
